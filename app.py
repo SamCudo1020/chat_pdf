a6d6631754f1f7e949b993223a7de08a40bf72eb
SamCudo1020 <chamuel567@gmail.com>
2026-04-09 10:35:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.write("Versión de Python:", platform.python_version())
 
 # Load and display image
 try:
-    image = Image.open('araña.jpg')
+    image = Image.open('arana.jpg')
     st.image(image, width=350)
 except Exception as e:
     st.warning(f"No se pudo cargar la imagen: {e}")
